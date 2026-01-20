c8970db4a21455aab4b12b9f4ea288a1f470e903
panchamipanchu68-coder <panchamipanchu68@gmail.com>
2026-01-20 21:00:01 +0530

    Add alert on form submission

---
+++ script.js
@@ -0,0 +1,5 @@
+// Simple alert on form submit
+document.querySelector("form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Thank you! We will contact you soon.");
+});
